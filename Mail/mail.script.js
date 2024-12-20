25e398e206b92c4afc8c3781b0b91d0edfacff72
nicolo_canevari <nico.canevari@outlook.it>
2024-12-20 15:46:18 +0100

    Esercizio mail parte 2

--- Mail/mail.script.js
+++ Mail/mail.script.js
@@ -6,7 +6,7 @@ const listaInvitati = [
     "luigi.sarto@example.com",
     "paola.novari@example.com",
     "giovanni.crippa@example.com"
-    
+
 ];
 
 // Funzione per chiedere l'email all'utente e verificare se è nella lista
@@ -15,4 +15,35 @@ function controllaAccesso() {
     // Chiedi all'utente di inserire la sua email
     const emailUtente = prompt("Inserisci la tua email:");
 
+    // Verifico per sapere se l'email si trovi nella lista
+    let emailTrovata = false;
+
+    // Ciclo per controllare se l'email è nella lista
+    for (let i = 0; i < listaInvitati.length; i++) {
+
+        if (emailUtente === listaInvitati[i]) {
+            // assegno un valore true alla variabile emailTrovata
+            emailTrovata = true;
+
+            // uso break per uscire da for, se la mail viene trovata il programma cessa subito il ciclo e non attua altri confonti tra emailUtente e listaInvitati
+            break;
+
+        }
+
+    }
+
+    // Mostra un messaggio a seconda del risultato utilizzando alert con disclaimer
+    if (emailTrovata) {
+
+        alert("Benvenuto alla festa!");
+
+    } else {
+
+        alert("Mi dispiace, la tua email non è nella lista degli invitati.");
+
+    }
+
 }
+
+// Output verifica mailUtente
+controllaAccesso();
