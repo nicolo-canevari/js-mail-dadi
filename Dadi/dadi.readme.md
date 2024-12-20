b429c183e9882708ec030103467bb6c04a153000
nicolo_canevari <nico.canevari@outlook.it>
2024-12-20 15:14:18 +0100

    Esercizio dadi  parte 2 js

--- Dadi/dadi.readme.md
+++ Dadi/dadi.readme.md
@@ -2,14 +2,12 @@ GIOCO DEI DADI:
 
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer. Stabilire il vincitore, in base a chi fa il punteggio più alto.
 Prima di partire a scrivere codice poniamoci qualche domanda:
-Che ci sia un array da qualche parte?
-Se dobbiamo confrontare qualcosa che “cosa” ci serve?
 
 LOGICA:
 
 -Generazione dei numeri casuali: 
  Sia il giocatore che il computer lanciano un dado, ogni lancio restituirà un numero tra 1 e 6.
- 
+
 -Confronto: 
  Confrontiamo il punteggio del giocatore con quello del computer.
 
--- Dadi/dadi.script.js
+++ Dadi/dadi.script.js
@@ -1,8 +1,29 @@
 // Funzione per generare un numero casuale da 1 a 6
 function lanciaDado() {
+
     return Math.floor(Math.random() * 6) + 1;
+
 }
 
 // Simula il lancio del dado per il giocatore e per il computer
 let dadoGiocatore = lanciaDado();
 let dadoComputer = lanciaDado();
+
+// Visualizza i risultati
+console.log("Giocatore ha ottenuto: " + dadoGiocatore);
+console.log("Computer ha ottenuto: " + dadoComputer);
+
+// Determina il vincitore 
+if (dadoGiocatore > dadoComputer) {
+
+    console.log("Il Giocatore vince!");
+
+} else if (dadoGiocatore < dadoComputer) {
+
+    console.log("Il Computer vince!");
+
+} else {
+
+    console.log("È un pareggio!");
+    
+}
